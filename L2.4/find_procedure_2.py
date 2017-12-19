7f21ae4cc5f1ac43b0c45ffa420f679344264eb7
Elena Yakimets <elenaxp@yandex.ru>
2017-12-19 19:35:36 +0300

    no message

---
+++ L2.4/find_procedure_2.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# # Задание
+# 7  # мне нужно отыскать файл среди десятков других
+# 8  # я знаю некоторые части этого файла (на память или из другого источника)
+# 9  # я ищу только среди .sql файлов
+# 10  # 1. программа ожидает строку, которую будет искать (input())
+# 11  # после того, как строка введена, программа ищет её во всех файлах
+# 12  # выводит список найденных файлов построчно
+# 13  # выводит количество найденных файлов
+# 14  # 2. снова ожидает ввод
+# 15  # поиск происходит только среди найденных на этапе 1
+# 16  # 3. снова ожидает ввод
+# 17  # ...
+# 18  # Выход из программы программировать не нужно.
+# 19  # Достаточно принудительно остановить, для этого можете нажать Ctrl + C
+# 20
+#
+# 21  # Пример на настоящих данных
+# 22
+#
+# 23  # python3 find_procedure.py
+# 24  # Введите строку: INSERT
+# 25  # ... большой список файлов ...
+# 26  # Всего: 301
+# 27  # Введите строку: APPLICATION_SETUP
+# 28  # ... большой список файлов ...
+# 29  # Всего: 26
+# 30  # Введите строку: A400M
+# 31  # ... большой список файлов ...
+# 32  # Всего: 17
+# 33  # Введите строку: 0.0
+# 34  # Migrations/000_PSE_Application_setup.sql
+# 35  # Migrations/100_1-32_PSE_Application_setup.sql
+# 36  # Всего: 2
+# 37  # Введите строку: 2.0
+# 38  # Migrations/000_PSE_Application_setup.sql
+# 39  # Всего: 1
+# 40
+# 41  # не забываем организовывать собственный код в функции
+
+
+import os
+
+migrations = 'Migrations'
+current_dir = os.path.dirname(os.path.abspath(__file__))
+search_dir = os.path.join(current_dir, migrations)
+ext = '.sql'
+files = [search_dir + "\\" + i for i in os.listdir(search_dir) if i.endswith(ext)]
+list_my = []
+
+
+def f_search(files):
+    list_my = []
+    s = input('Filter:')
+    for fname in files:
+        with open(fname, encoding='utf-8') as f:
+            if s in f.read():
+                print(fname)
+                list_my.append(fname)
+    print('Файлов всего', len(list_my))
+    return list_my
+
+
+if __name__ == '__main__':
+    while True:
+        files = f_search(files)
